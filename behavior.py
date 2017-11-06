e61c2f86aae48b3a897667eccdce3114882ba14e
Håkon Collett Bjørgan <macbookpro@dhcp-10-24-18-214.wlan.ntnu.no>
2017-11-06 14:43:44 +0100

    utkast til behavior

---
+++ behavior.py
@@ -0,0 +1,30 @@
+
+
+class Behavior:
+
+    def __init__(self, bbcon, sensob):
+        self.bbcon = bbcon
+        self.sensob = sensob
+        self.motor_recommandations = []
+        self.active_flag = False
+        self.halt_request = False
+        self.priority
+        self.match_degree
+        self.weight
+
+    def consider_deactivation(self):
+        if self.active_flag:
+            pass
+
+    def consider_activation(self):
+        if not self.active_flag:
+            pass
+
+    def update(self):
+        pass
+
+    def sense_and_act(self):
+        pass
+
+
+
